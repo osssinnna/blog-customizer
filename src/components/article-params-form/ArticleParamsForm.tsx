ac5406907247c84a1ca3f2c0d587da1cdf7415da
Darya <127752063+osssinnna@users.noreply.github.com>
2025-11-30 19:54:41 +0300

    without overlay

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 
 import {
@@ -28,11 +28,13 @@ export const ArticleParamsForm = ({
 	articleState,
 	onApply,
 }: ArticleParamsFormProps) => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isFormOpen, setIsFormOpen] = useState(false);
 	const [formState, setFormState] = useState<ArticleStateType>(articleState);
 
-	const handleToggleSidebar = () => {
-		setIsOpen((prev) => !prev);
+	const panelRef = useRef<HTMLDivElement | null>(null);
+
+	const handleToggleForm = () => {
+		setIsFormOpen((prev) => !prev);
 	};
 
 	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -50,102 +52,126 @@ export const ArticleParamsForm = ({
 		setFormState(articleState);
 	}, [articleState]);
 
+	useEffect(() => {
+		if (!isFormOpen) {
+			return;
+		}
+
+		const handleClick = (event: MouseEvent) => {
+			if (!panelRef.current) return;
+
+			const target = event.target as Node;
+
+			if (panelRef.current.contains(target)) return;
+
+			setIsFormOpen(false);
+		};
+
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setIsFormOpen(false);
+			}
+		};
+
+		window.addEventListener('mousedown', handleClick);
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('mousedown', handleClick);
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [isFormOpen]);
+
 	return (
-		<>
-			{isOpen && (
-				<div className={styles.overlay} onClick={() => setIsOpen(false)} />
-			)}
+		<div className={styles.panel} ref={panelRef}>
+			<ArrowButton isOpen={isFormOpen} onClick={handleToggleForm} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isFormOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text
+						as='h2'
+						size={31}
+						weight={800}
+						uppercase
+						align='left'
+						family='open-sans'>
+						Задайте параметры
+					</Text>
 
-			<div className={styles.panel}>
-				<ArrowButton isOpen={isOpen} onClick={handleToggleSidebar} />
-				<aside
-					className={clsx(styles.container, {
-						[styles.container_open]: isOpen,
-					})}>
-					<form
-						className={styles.form}
-						onSubmit={handleSubmit}
-						onReset={handleReset}>
-						<Text
-							as='h2'
-							size={31}
-							weight={800}
-							uppercase
-							align='left'
-							family='open-sans'>
-							Задайте параметры
-						</Text>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								fontFamilyOption: option,
+							}))
+						}
+					/>
 
-						<Select
-							title='Шрифт'
-							selected={formState.fontFamilyOption}
-							options={fontFamilyOptions}
-							onChange={(option) =>
-								setFormState((prev) => ({
-									...prev,
-									fontFamilyOption: option,
-								}))
-							}
-						/>
+					<RadioGroup
+						name='font-size'
+						title='Размер шрифта'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								fontSizeOption: option,
+							}))
+						}
+					/>
 
-						<RadioGroup
-							name='font-size'
-							title='Размер шрифта'
-							selected={formState.fontSizeOption}
-							options={fontSizeOptions}
-							onChange={(option) =>
-								setFormState((prev) => ({
-									...prev,
-									fontSizeOption: option,
-								}))
-							}
-						/>
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								fontColor: option,
+							}))
+						}
+					/>
 
-						<Select
-							title='Цвет шрифта'
-							selected={formState.fontColor}
-							options={fontColors}
-							onChange={(option) =>
-								setFormState((prev) => ({
-									...prev,
-									fontColor: option,
-								}))
-							}
-						/>
+					<Separator />
 
-						<Separator />
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								backgroundColor: option,
+							}))
+						}
+					/>
 
-						<Select
-							title='Цвет фона'
-							selected={formState.backgroundColor}
-							options={backgroundColors}
-							onChange={(option) =>
-								setFormState((prev) => ({
-									...prev,
-									backgroundColor: option,
-								}))
-							}
-						/>
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								contentWidth: option,
+							}))
+						}
+					/>
 
-						<Select
-							title='Ширина контента'
-							selected={formState.contentWidth}
-							options={contentWidthArr}
-							onChange={(option) =>
-								setFormState((prev) => ({
-									...prev,
-									contentWidth: option,
-								}))
-							}
-						/>
-
-						<div className={styles.bottomContainer}>
-							<Button title='Сбросить' htmlType='reset' type='clear' />
-							<Button title='Применить' htmlType='submit' type='apply' />
-						</div>
-					</form>
-				</aside>
-			</div>
-		</>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
+		</div>
 	);
 };
